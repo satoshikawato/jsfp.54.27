e1bba42864bd7c828a1b0d049c918c2d9c7cf43d
satoshikawato <58936715+satoshikawato@users.noreply.github.com>
2019-12-20 11:13:59 +0900

    Update bowtie2_to_pilon.sh

--- bowtie2_to_pilon.sh
+++ bowtie2_to_pilon.sh
@@ -1,12 +1,12 @@
-cd /home/genome/Desktop/Kawato/181011_kwt/bowtie2/
-/home/genome/kawato/bowtie2/bowtie2-2.3.4.1-linux-x86_64/bowtie2-build \
+
+bowtie2-build \
 Bs${num}_pilon.fasta Bs${num}_pilon
 for num in 01 02 03 04 05 06 07 08 09 10 11
 do
 
-/home/genome/kawato/bowtie2/bowtie2-2.3.4.1-linux-x86_64/bowtie2-build \
+bowtie2-build \
 Bs${num}_pilon.fasta Bs${num}_pilon
-/home/genome/kawato/bowtie2/bowtie2-2.3.4.1-linux-x86_64/bowtie2 \
+bowtie2 \
 -x Bs${num}_pilon \
 -1 Bs${num}_R1.fastq.gz \
 -2 Bs${num}_R2.fastq.gz \
@@ -15,7 +15,7 @@ Bs${num}_pilon.fasta Bs${num}_pilon
 samtools index Bs${num}_pilon_sorted.bam
 
 
-java -Xmx110G -jar /home/genome/kawato/pilon-1.22.jar \
+java -Xmx110G -jar pilon.jar \
 --genome Bs${num}_pilon.fasta \
 --frags Bs${num}_pilon_sorted.bam \
 --output Bs${num}_pilon2 \
